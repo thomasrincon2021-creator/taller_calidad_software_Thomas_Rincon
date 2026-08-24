943f86b9b9eb5fad0263b43a0ea09a18381867f8
thomasrincon2021-creator <thomasrincon2021@gmail.com>
2026-08-24 15:56:54 -0500

    Delete repository directory

--- repository/ProductoRepositorio.java
+++
@@ -1,9 +0,0 @@
-package com.nowstyle.taller_calidad_backend.repository;
-
-import com.nowstyle.taller_calidad_backend.modelo.Producto;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductoRepositorio extends JpaRepository<Producto, Long> {
-}
