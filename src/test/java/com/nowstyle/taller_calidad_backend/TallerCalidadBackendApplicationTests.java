44ae04499c29dd16499e2955c19ed40af68b61e9
thomasrincon2021-creator <thomasrincon2021@gmail.com>
2026-09-14 22:10:10 -0500

    test: configurar perfil de pruebas

--- src/test/java/com/nowstyle/taller_calidad_backend/TallerCalidadBackendApplicationTests.java
+++ src/test/java/com/nowstyle/taller_calidad_backend/TallerCalidadBackendApplicationTests.java
@@ -2,13 +2,14 @@ package com.nowstyle.taller_calidad_backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TallerCalidadBackendApplicationTests {
 
     @Test
     void contextLoads() {
-        // Este test valida que el contexto de la aplicación Spring Boot
-        // cargue correctamente sin errores.
+        // Este test valida que el contexto de la aplicación cargue correctamente.
     }
 }
